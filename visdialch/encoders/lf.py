7db6246fae613a5abf1fbea0b2b987b615ae5a13
Karan Desai <karandesai281196@gmail.com>
2018-12-26 16:10:34 -0500

    Add support in LF encoder for bottom-up features (mean pooling).

--- visdialch/encoders/lf.py
+++ visdialch/encoders/lf.py
@@ -9,19 +9,23 @@ class LateFusionEncoder(nn.Module):
         super().__init__()
         self.config = config
 
-        self.word_embed = nn.Embedding(config["vocab_size"],
-                                       config["word_embedding_size"],
-                                       padding_idx=0)
-        self.hist_rnn = nn.LSTM(config["word_embedding_size"],
-                                config["lstm_hidden_size"],
-                                config["lstm_num_layers"],
-                                batch_first=True,
-                                dropout=config["dropout"])
-        self.ques_rnn = nn.LSTM(config["word_embedding_size"],
-                                config["lstm_hidden_size"],
-                                config["lstm_num_layers"],
-                                batch_first=True,
-                                dropout=config["dropout"])
+        self.word_embed = nn.Embedding(
+            config["vocab_size"], config["word_embedding_size"], padding_idx=0
+        )
+        self.hist_rnn = nn.LSTM(
+            config["word_embedding_size"],
+            config["lstm_hidden_size"],
+            config["lstm_num_layers"],
+            batch_first=True,
+            dropout=config["dropout"],
+        )
+        self.ques_rnn = nn.LSTM(
+            config["word_embedding_size"],
+            config["lstm_hidden_size"],
+            config["lstm_num_layers"],
+            batch_first=True,
+            dropout=config["dropout"],
+        )
         self.dropout = nn.Dropout(p=config["dropout"])
 
         # questions and history are right padded sequences of variable length
@@ -40,27 +44,34 @@ class LateFusionEncoder(nn.Module):
         nn.init.constant_(self.fusion.bias, 0)
 
     def forward(self, batch):
-        img = batch['img_feat']
-        ques = batch['ques']
-        hist = batch['hist']
+        # shape: (batch, img_feature_size) - CNN fc7 features
+        # shape: (batch, num_proposals, img_feature_size) - RCNN bottom-up features
+        img = batch["img_feat"]
+        # shape: (batch, 10, max_sequence_length)
+        ques = batch["ques"]
+        # shape: (batch, 10, max_sequence_length * 2 * 10), concatenated qa * 10 rounds
+        hist = batch["hist"]
+        # num_rounds = 10, even for test split (padded dialog rounds at the end)
+        batch_size, num_rounds, max_sequence_length = ques.size()
 
-        batch_size, num_rounds, _ = ques.size()
+        # average bottom-up features of all proposals, to form fc7-like features
+        if img.dim() == 3:
+            img = torch.mean(img, dim=1)  # shape: (batch, img_feature_size)
 
         # repeat image feature vectors to be provided for every round
         img = img.view(batch_size, 1, self.config["img_feature_size"])
         img = img.repeat(1, num_rounds, 1)
-        img = img.view(batch_size * num_rounds,
-                       self.config["img_feature_size"])
+        img = img.view(batch_size * num_rounds, self.config["img_feature_size"])
 
         # embed questions
-        ques = ques.view(-1, ques.size(2))
+        ques = ques.view(batch_size * num_rounds, max_sequence_length)
         ques_embed = self.word_embed(ques)
-        ques_embed = self.ques_rnn(ques_embed, batch['ques_len'])
+        ques_embed = self.ques_rnn(ques_embed, batch["ques_len"])
 
         # embed history
-        hist = hist.view(-1, hist.size(2))
+        hist = hist.view(batch_size * num_rounds, max_sequence_length * 20)
         hist_embed = self.word_embed(hist)
-        hist_embed = self.hist_rnn(hist_embed, batch['hist_len'])
+        hist_embed = self.hist_rnn(hist_embed, batch["hist_len"])
 
         fused_vector = torch.cat((img, ques_embed, hist_embed), 1)
         fused_vector = self.dropout(fused_vector)
