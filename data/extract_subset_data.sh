556def30bbce2fadb6a818637b21d29a89ec1a9f
Karan Desai <karandesai281196@gmail.com>
2019-07-25 03:17:15 -0400

    Remove legacy files after parallel tokenizzation merge.

--- data/extract_subset_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export CURRENT_DIR=${PWD}
-export PARENT_DIR="$(dirname "$CURRENT_DIR")"
-export PROJECT_DIR="$(dirname "$PARENT_DIR")"
-export DATA_DIR=$PROJECT_DIR/data
-
-python extract_subset_data.py \
--d $DATA_DIR
