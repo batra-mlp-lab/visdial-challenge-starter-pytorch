026ae82ada92ea25927448ab20f6321d610adeb8
Zhihao Zhou <zhouzhihao90@gmail.com>
2019-11-03 12:50:41 -0500

    init

--- visdialch/encoders/__init__.py
+++ visdialch/encoders/__init__.py
@@ -1,6 +1,10 @@
 from visdialch.encoders.lf import LateFusionEncoder
+from visdialch.encoders.rva import RvAEncoder
 
 
 def Encoder(model_config, *args):
-    name_enc_map = {"lf": LateFusionEncoder}
+    name_enc_map = {
+    	"lf": LateFusionEncoder,
+    	"rva": RvAEncoder,
+    }
     return name_enc_map[model_config["encoder"]](model_config, *args)
