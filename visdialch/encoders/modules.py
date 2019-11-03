56ae2ccf55822d57c3aeccf1011bb22d3470a035
Zhihao Zhou <zhouzhihao90@gmail.com>
2019-11-03 12:21:12 -0500

    rva

---
+++ visdialch/encoders/modules.py
@@ -0,0 +1,247 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from visdialch.utils import GatedTrans
+
+class ATT_MODULE(nn.Module):
+    """docstring for ATT_MODULE"""
+    def __init__(self, config):
+        super(ATT_MODULE, self).__init__()
+
+        self.V_embed = nn.Sequential(
+            nn.Dropout(p=config["dropout_fc"]),
+            GatedTrans(
+                config["img_feature_size"],
+                config["lstm_hidden_size"]
+            ),
+        )
+        self.Q_embed = nn.Sequential(
+            nn.Dropout(p=config["dropout_fc"]),
+            GatedTrans(
+                config["word_embedding_size"], 
+                config["lstm_hidden_size"]
+            ),
+        )
+        self.att = nn.Sequential(
+            nn.Dropout(p=config["dropout_fc"]),
+            nn.Linear(
+                config["lstm_hidden_size"],
+                1
+            )
+        )
+
+        self.softmax = nn.Softmax(dim=-1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight.data)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias.data, 0)
+        
+    def forward(self, img, ques):
+        # input
+        # img - shape: (batch_size, num_proposals, img_feature_size)
+        # ques - shape: (batch_size, num_rounds, word_embedding_size)
+        # output
+        # att - shape: (batch_size, num_rounds, num_proposals)
+
+        batch_size = ques.size(0)
+        num_rounds = ques.size(1)
+        num_proposals = img.size(1)
+
+        img_embed = img.view(-1, img.size(-1)) # shape: (batch_size * num_proposals, img_feature_size)
+        img_embed = self.V_embed(img_embed) # shape: (batch_size, num_proposals, lstm_hidden_size)
+        img_embed = img_embed.view(batch_size, num_proposals, img_embed.size(-1)) # shape: (batch_size, num_proposals, lstm_hidden_size)
+        img_embed = img_embed.unsqueeze(1).repeat(1, num_rounds, 1, 1) # shape: (batch_size, num_rounds, num_proposals, lstm_hidden_size)
+        
+        ques_embed = ques.view(-1, ques.size(-1)) # shape: (batch_size * num_rounds, word_embedding_size)
+        ques_embed = self.Q_embed(ques_embed) # shape: (batch_size, num_rounds, lstm_hidden_size)
+        ques_embed = ques_embed.view(batch_size, num_rounds, ques_embed.size(-1)) # shape: (batch_size, num_rounds, lstm_hidden_size)
+        ques_embed = ques_embed.unsqueeze(2).repeat(1, 1, num_proposals, 1) # shape: (batch_size, num_rounds, num_proposals, lstm_hidden_size)
+        
+        att_embed = F.normalize(img_embed * ques_embed, p=2, dim=-1) # (batch_size, num_rounds, num_proposals, lstm_hidden_size)
+        att_embed = self.att(att_embed).squeeze(-1) # (batch_size, num_rounds, num_proposals)
+        att = self.softmax(att_embed) # shape: (batch_size, num_rounds, num_proposals)
+        
+        return att
+
+class PAIR_MODULE(nn.Module):
+    """docstring for PAIR_MODULE"""
+    def __init__(self, config):
+        super(PAIR_MODULE, self).__init__()
+        
+        self.H_embed = nn.Sequential(
+            nn.Dropout(p=config["dropout_fc"]),
+            GatedTrans(
+                config["lstm_hidden_size"]*2,
+                config["lstm_hidden_size"]),
+        )
+        self.Q_embed = nn.Sequential(
+            nn.Dropout(p=config["dropout_fc"]),
+            GatedTrans(
+                config["lstm_hidden_size"]*2,
+                config["lstm_hidden_size"]),
+        )
+        self.MLP = nn.Sequential(
+            nn.Dropout(p=config["dropout_fc"]),
+            nn.Linear(
+                config["lstm_hidden_size"]*2,
+                config["lstm_hidden_size"]),
+            nn.Dropout(p=config["dropout_fc"]),
+            nn.Linear(
+                config["lstm_hidden_size"],
+                1
+            )
+        )
+        self.att = nn.Linear(2, 1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight.data)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias.data, 0)
+
+    def forward(self, hist, ques):
+        # input
+        # ques shape: (batch_size, num_rounds, lstm_hidden_size*2)
+        # hist shape: (batch_size, num_rounds, lstm_hidden_size*2)
+        # output
+        # hist_gs_set - shape: (batch_size, num_rounds, num_rounds)
+
+        batch_size = ques.size(0)
+        num_rounds = ques.size(1)
+        
+        hist_embed = self.H_embed(hist) # shape: (batch_size, num_rounds, lstm_hidden_size)
+        hist_embed = hist_embed.unsqueeze(1).repeat(1, num_rounds, 1, 1) # shape: (batch_size, num_rounds, num_rounds, lstm_hidden_size)
+        
+        ques_embed = self.Q_embed(ques) # shape: (batch_size, num_rounds, lstm_hidden_size)
+        ques_embed = ques_embed.unsqueeze(2).repeat(1, 1, num_rounds, 1) # shape: (batch_size, num_rounds, num_rounds, lstm_hidden_size)
+        
+        att_embed = torch.cat((hist_embed, ques_embed), dim=-1)
+        score = self.MLP(att_embed)
+
+        delta_t = torch.tril(torch.ones(size=[num_rounds, num_rounds], requires_grad=False)).cumsum(dim=0) # (num_rounds, num_rounds)
+        delta_t = delta_t.view(1, num_rounds, num_rounds, 1).repeat(batch_size, 1, 1, 1) # (batch_size, num_rounds, num_rounds, 1)
+        delta_t = delta_t.cuda()
+        att_embed = torch.cat((score, delta_t), dim=-1) # (batch_size, num_rounds, num_rounds, lstm_hidden_size*2)
+        
+        hist_logits = self.att(att_embed).squeeze(-1) # (batch_size, num_rounds, num_rounds)
+
+        # PAIR
+        hist_gs_set = torch.zeros_like(hist_logits)
+        for i in range(num_rounds):
+            # one-hot
+            logits = hist_logits[:, i, :(i+1)]
+            if self.training:
+                hist_gs = F.gumbel_softmax(logits, hard=True) # shape: (batch_size, i+1)
+            else:
+                _, max_value_indexes = logits.detach().max(1, keepdim=True)
+                hist_gs = logits.detach().clone().zero_().scatter_(1, max_value_indexes, 1)
+            hist_gs_set[:, i, :(i+1)] = hist_gs
+
+        return hist_gs_set
+
+class INFER_MODULE(nn.Module):
+    """docstring for INFER_MODULE"""
+    def __init__(self, config):
+        super(INFER_MODULE, self).__init__()
+
+        self.embed = nn.Sequential(
+            nn.Dropout(p=config["dropout_fc"]),
+            GatedTrans(
+                config["word_embedding_size"], 
+                config["lstm_hidden_size"]),
+        )
+        self.att = nn.Sequential(
+            nn.Dropout(p=config["dropout_fc"]),
+            nn.Linear(
+                config["lstm_hidden_size"], 
+                2
+            )
+        )
+
+        self.softmax = nn.Softmax(dim=-1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight.data)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias.data, 0)
+        
+    def forward(self, ques):
+        # input
+        # ques - shape: (batch_size, num_rounds, word_embedding_size)
+        # output
+        # ques_gs - shape: (batch_size, num_rounds, 2)
+        # Lambda - shape: (batch_size, num_rounds, 2)
+
+        batch_size = ques.size(0)
+        num_rounds = ques.size(1)
+
+        ques_embed = self.embed(ques) # shape: (batch_size, num_rounds, quen_len_max, lstm_hidden_size)
+        ques_embed = F.normalize(ques_embed, p=2, dim=-1) # shape: (batch_size, num_rounds, quen_len_max, lstm_hidden_size) 
+        ques_logits = self.att(ques_embed) # shape: (batch_size, num_rounds, 2)
+        
+        logits = ques_logits.view(-1, 2)
+        if self.training:
+            ques_gs = F.gumbel_softmax(logits, hard=True) # shape: (batch_size, i+1)
+        else:
+            _, max_value_indexes = logits.detach().max(1, keepdim=True)
+            ques_gs = logits.detach().clone().zero_().scatter_(1, max_value_indexes, 1)
+        ques_gs = ques_gs.view(-1, num_rounds, 2)
+
+        Lambda = self.softmax(ques_logits)
+        
+        return ques_gs, Lambda
+
+class RvA_MODULE(nn.Module):
+    """docstring for R_CALL"""
+    def __init__(self, config):
+        super(RvA_MODULE, self).__init__()
+
+        self.INFER_MODULE = INFER_MODULE(config)
+        self.PAIR_MODULE = PAIR_MODULE(config)
+        self.ATT_MODULE = ATT_MODULE(config)
+        
+    def forward(self, img, ques, hist):
+        # img shape: [batch_size, num_proposals, i_dim]
+        # img_att_ques shape: [batch_size, num_rounds, num_proposals]
+        # img_att_cap shape: [batch_size, 1, num_proposals]
+        # ques_gs shape: [batch_size, num_rounds, 2]
+        # hist_logits shape: [batch_size, num_rounds, num_rounds]
+        # ques_gs_prob shape: [batch_size, num_rounds, 2]
+
+        cap_feat, ques_feat, ques_encoded = ques
+
+        batch_size = ques_feat.size(0)
+        num_rounds = ques_feat.size(1)
+        num_proposals = img.size(1)
+
+        ques_gs, ques_gs_prob = self.INFER_MODULE(ques_feat) # (batch_size, num_rounds, 2)
+        hist_gs_set = self.PAIR_MODULE(hist, ques_encoded)
+        img_att_ques = self.ATT_MODULE(img, ques_feat)
+        img_att_cap  = self.ATT_MODULE(img, cap_feat)
+
+        # soft
+        ques_prob_single = torch.Tensor(data=[1, 0]).view(1, -1).repeat(batch_size, 1) # shape: [batch_size, 2]
+        ques_prob_single = ques_prob_single.cuda()
+        ques_prob_single.requires_grad = False
+        
+        img_att_refined = img_att_ques.data.clone().zero_() # shape: [batch_size, num_rounds, num_proposals]
+        for i in range(num_rounds):
+            if i == 0:
+                img_att_temp = img_att_cap.view(-1, img_att_cap.size(-1)) # shape: [batch_size, num_proposals]
+            else:
+                hist_gs = hist_gs_set[:, i, :(i+1)] # shape: [batch_size, i+1]            
+                img_att_temp = torch.cat((img_att_cap, img_att_refined[:, :i, :]), dim=1) # shape: [batch_size, i+1, num_proposals]
+                img_att_temp = torch.sum(hist_gs.unsqueeze(-1) * img_att_temp, dim=-2) # shape: [batch_size, num_proposals]
+            img_att_cat = torch.cat((img_att_ques[:, i, :].unsqueeze(1), img_att_temp.unsqueeze(1)), dim=1) # shape: [batch_size ,2, num_proposals]
+            # soft
+            ques_prob_pair = ques_gs_prob[:, i, :]
+            ques_prob = torch.cat((ques_prob_single, ques_prob_pair), dim=-1) # shape: [batch_size, 2]
+            ques_prob = ques_prob.view(-1, 2, 2) # shape: [batch_size, 2, 2]
+            ques_prob_refine = torch.bmm(ques_gs[:, i, :].view(-1, 1, 2), ques_prob).view(-1, 1, 2) # shape: [batch_size, num_rounds, 2]
+        
+            img_att_refined[:, i, :] = torch.bmm(ques_prob_refine, img_att_cat).view(-1, num_proposals) # shape: [batch_size, num_proposals]
+        
+        return img_att_refined, (ques_gs, hist_gs_set, img_att_ques)
---
+++ visdialch/encoders/rva.py
@@ -0,0 +1,170 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from visdialch.utils import DynamicRNN
+from visdialch.utils import Q_ATT, H_ATT, V_Filter
+from .modules import RvA_MODULE
+
+class RvAEncoder(nn.Module):
+    def __init__(self, config, vocabulary):
+        super().__init__()
+        self.config = config
+
+        self.word_embed = nn.Embedding(
+            len(vocabulary), 
+            config["word_embedding_size"], 
+            padding_idx=vocabulary.PAD_INDEX
+        )
+
+        self.hist_rnn = nn.LSTM(
+            config["word_embedding_size"],
+            config["lstm_hidden_size"],
+            config["lstm_num_layers"],
+            batch_first=True,
+            dropout=config["dropout"], 
+            bidirectional=True
+        )
+        self.ques_rnn = nn.LSTM(
+            config["word_embedding_size"],
+            config["lstm_hidden_size"],
+            config["lstm_num_layers"],
+            batch_first=True,
+            dropout=config["dropout"], 
+            bidirectional=True
+        )        
+        # questions and history are right padded sequences of variable length
+        # use the DynamicRNN utility module to handle them properly
+        self.hist_rnn = DynamicRNN(self.hist_rnn)
+        self.ques_rnn = DynamicRNN(self.ques_rnn)
+        
+        # self attention for question
+        self.Q_ATT_ans = Q_ATT(config)
+        self.Q_ATT_ref = Q_ATT(config)
+        # question-based history attention
+        self.H_ATT_ans = H_ATT(config)
+
+        # modules
+        self.RvA_MODULE = RvA_MODULE(config)
+        self.V_Filter = V_Filter(config)
+
+        # fusion layer
+        self.fusion = nn.Sequential(
+            nn.Dropout(p=config["dropout_fc"]),
+            nn.Linear(
+                config["img_feature_size"] + config["word_embedding_size"] + config["lstm_hidden_size"] * 2, 
+                config["lstm_hidden_size"]
+            )
+        )
+        # other useful functions
+        self.softmax = nn.Softmax(dim=-1)
+
+        # initialization
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform(m.weight.data)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias.data, 0)
+
+    def forward(self, batch, return_att=False):
+        # img - shape: (batch_size, num_proposals, img_feature_size) - RCNN bottom-up features
+        img = batch["img_feat"]
+        batch_size = batch["ques"].size(0)
+        num_rounds = batch["ques"].size(1)
+
+        # init language embedding
+        # ques_word_embed - shape: (batch_size, num_rounds, quen_len_max, word_embedding_size)
+        # ques_word_encoded - shape: (batch_size, num_rounds, quen_len_max, lstm_hidden_size)
+        # ques_not_pad - shape: (batch_size, num_rounds, quen_len_max)
+        # ques_encoded - shape: (batch_size, num_rounds, lstm_hidden_size)
+        ques_word_embed, ques_word_encoded, ques_not_pad, ques_encoded = self.init_q_embed(batch)
+        # hist_word_embed - shape: (batch_size, num_rounds, quen_len_max, word_embedding_size)
+        # hist_encoded - shape: (batch_size, num_rounds, lstm_hidden_size)
+        hist_word_embed, hist_encoded = self.init_h_embed(batch)
+        # cap_word_embed - shape: (batch_size, num_rounds, quen_len_max, word_embedding_size)
+        # cap_word_encoded - shape: (batch_size, num_rounds, quen_len_max, lstm_hidden_size)
+        # cap_not_pad - shape: (batch_size, num_rounds, quen_len_max)
+        cap_word_embed, cap_word_encoded, cap_not_pad = self.init_cap_embed(batch)
+
+        # question feature for RvA
+        # ques_ref_feat - shape: (batch_size, num_rounds, lstm_hidden_size)
+        # ques_ref_att - shape: (batch_size, num_rounds, quen_len_max)
+        ques_ref_feat, ques_ref_att = self.Q_ATT_ref(ques_word_embed, ques_word_encoded, ques_not_pad)
+        # cap_ref_feat - shape: (batch_size, num_rounds, lstm_hidden_size)
+        cap_ref_feat, _ = self.Q_ATT_ref(cap_word_embed, cap_word_encoded, cap_not_pad) 
+
+        # RvA module
+        ques_feat = (cap_ref_feat, ques_ref_feat, ques_encoded)
+        # img_att - shape: (batch_size, num_rounds, num_proposals)
+        img_att, att_set = self.RvA_MODULE(img, ques_feat, hist_encoded)
+        # img_feat - shape: (batch_size, num_rounds, img_feature_size)
+        img_feat = torch.bmm(img_att, img)
+        
+        # ans_feat for joint embedding
+        # hist_ans_feat - shape: (batch_size, num_rounds, lstm_hidden_size*2)
+        hist_ans_feat = self.H_ATT_ans(hist_encoded, ques_encoded)
+        # ques_ans_feat - shape: (batch_size, num_rounds, word_embedding_size)
+        # ques_ans_att - shape: (batch_size, num_rounds, quen_len_max)
+        ques_ans_feat, ques_ans_att = self.Q_ATT_ans(ques_word_embed, ques_word_encoded, ques_not_pad)
+        # img_ans_feat - shape: (batch_size, num_rounds, img_feature_size)
+        img_ans_feat  = self.V_Filter(img_feat, ques_ans_feat)
+
+        # joint embedding
+        fused_vector = torch.cat((img_ans_feat, ques_ans_feat, hist_ans_feat), -1)
+        # img_ans_feat - shape: (batch_size, num_rounds, lstm_hidden_size)
+        fused_embedding = torch.tanh(self.fusion(fused_vector))
+
+        if return_att:
+            return fused_embedding, att_set + (ques_ref_att, ques_ans_att)
+        else:
+            return fused_embedding
+
+    def init_q_embed(self, batch):
+        ques = batch["ques"] # shape: (batch_size, num_rounds, quen_len_max)
+        batch_size, num_rounds, _ = ques.size()
+        lstm_hidden_size = self.config["lstm_hidden_size"]
+        
+        # question feature
+        ques_not_pad = (ques!=0).float() # shape: (batch_size, num_rounds, quen_len_max)
+        ques = ques.view(-1, ques.size(-1)) # shape: (batch_size*num_rounds, quen_len_max)
+        ques_word_embed = self.word_embed(ques) # shape: (batch_size*num_rounds, quen_len_max, lstm_hidden_size)
+        ques_word_encoded, _ = self.ques_rnn(ques_word_embed, batch['ques_len']) # shape: (batch_size*num_rounds, quen_len_max, lstm_hidden_size*2)
+        quen_len_max = ques_word_encoded.size(1)
+        loc = batch['ques_len'].view(-1).cpu().numpy()-1
+        ques_encoded_forawrd = ques_word_encoded[range(num_rounds*batch_size), loc, :lstm_hidden_size] # shape: (batch_size*num_rounds, lstm_hidden_size) 
+        ques_encoded_backward = ques_word_encoded[:, 0, lstm_hidden_size:] # shape: (batch_size*num_rounds, lstm_hidden_size) 
+        ques_encoded = torch.cat((ques_encoded_forawrd, ques_encoded_backward), dim=-1)
+        ques_encoded = ques_encoded.view(-1, num_rounds, ques_encoded.size(-1)) # shape: (batch_size, num_rounds, lstm_hidden_size*2)
+        ques_word_encoded = ques_word_encoded.view(-1, num_rounds, quen_len_max, ques_word_encoded.size(-1)) # shape: (batch_size, num_rounds, quen_len_max, lstm_hidden_size)
+        ques_word_embed = ques_word_embed.view(-1, num_rounds, quen_len_max, ques_word_embed.size(-1)) # shape: (batch_size, num_rounds, quen_len_max, word_embedding_size)
+
+        return ques_word_embed, ques_word_encoded, ques_not_pad, ques_encoded
+
+    def init_h_embed(self, batch):
+        hist = batch["hist"] # shape: (batch_size, num_rounds, hist_len_max)
+        batch_size, num_rounds, _ = hist.size()
+        lstm_hidden_size = self.config["lstm_hidden_size"]
+        
+        hist = hist.view(-1, hist.size(-1)) # shape: (batch_size*num_rounds, hist_len_max)
+        hist_word_embed = self.word_embed(hist) # shape: (batch_size*num_rounds, hist_len_max, word_embedding_size)
+        hist_word_encoded, _ = self.hist_rnn(hist_word_embed, batch['hist_len']) # shape: (batch_size*num_rounds, hist_len_max, lstm_hidden_size*2)
+        loc = batch['hist_len'].view(-1).cpu().numpy()-1
+        hist_encoded_forward = hist_word_encoded[range(num_rounds*batch_size), loc, :lstm_hidden_size] # shape: (batch_size*num_rounds, hist_len_max, lstm_hidden_size*2) 
+        hist_encoded_backward = hist_word_encoded[:, 0, lstm_hidden_size:] # shape: (batch_size*num_rounds, lstm_hidden_size) 
+        hist_encoded = torch.cat((hist_encoded_forward, hist_encoded_backward), dim=-1)
+        hist_encoded = hist_encoded.view(-1, num_rounds, hist_encoded.size(-1)) # shape: (batch_size, num_rounds, lstm_hidden_size)
+
+        return hist_word_embed, hist_encoded
+
+    def init_cap_embed(self, batch):
+        cap = batch["hist"][:, :1, :] # shape: (batch_size, 1, hist_len_max)
+
+        # caption feature like question
+        cap_not_pad = (cap!=0).float() # shape: (batch_size, 1, hist_len_max)
+        cap_word_embed = self.word_embed(cap.squeeze(1)) # shape: (batch_size*1, hist_len_max, lstm_hidden_size)
+        cap_len = batch['hist_len'][:, :1]
+        cap_word_encoded, _ = self.ques_rnn(cap_word_embed, cap_len) # shape: (batch_size*1, hist_len_max, lstm_hidden_size)
+        cap_word_encoded = cap_word_encoded.unsqueeze(1) # shape: (batch_size, 1, hist_len_max, lstm_hidden_size)
+        cap_word_embed = cap_word_embed.unsqueeze(1) # shape: (batch_size, 1, hist_len_max, lstm_hidden_size)
+
+        return cap_word_embed, cap_word_encoded, cap_not_pad
